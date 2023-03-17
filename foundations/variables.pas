d980dbf5e7a965d504653ab4c7fd67cce883b215
Ryohskay <learnerryo@gmail.com>
2023-03-17 11:37:24 +0000

    added more notes about variable and assignment

--- foundations/variables.pas
+++ foundations/variables.pas
@@ -2,10 +2,19 @@ program variables;
 {$mode fpc}
 
 var
-	num: Integer;
+	num: Integer = 20;
+	{ special global assignment available, used only 
+	because global variable will automatically initialised with a default value anyway }
+	prefix: string;
+	msg: string;
+	IsCorrect: Boolean;
 
 begin
-	num := 20;
 	num := $1a;  { hex integer }
 	num := %10;  { binary integer }
+	prefix := 'The author''s name';  { doubling quotes escapes the quote character }
+	{ sys constant sLineBreak is system-dependent newline char }
+	msg := prefix + sLineBreak + 'Marco Cantu''';
+	IsCorrect := True;
+	WriteLn(msg);
 end.
