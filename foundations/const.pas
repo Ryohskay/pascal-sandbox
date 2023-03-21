2924b026a770bbc5b38284d50674aff1d2c13198
Ryohskay <learnerryo@gmail.com>
2023-03-21 23:57:02 +0000

    updated const example

--- foundations/const.pas
+++ foundations/const.pas
@@ -2,13 +2,19 @@ program Constant;
 {$mode fpc}
 {$WriteableConst OFF}
 
+uses SysUtils;
+
 const
 	Thousand = 1000;
 	Name = 'Ryohskay';
+
+var
+	item: Integer = 10;
 
 resourcestring
 	AuthorName = 'Ryohskay N.';
 
 begin
 	WriteLn(Thousand);
+	WriteLn(IntToStr(item));
 end.
