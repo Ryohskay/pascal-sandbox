7e5e5f1415db460221262aacc64e558775415ec5
Ryohskay on Pop <learnerryo@gmail.com>
2023-03-24 23:41:03 +0000

    improved comments

--- algos/fibo-for.pas
+++ algos/fibo-for.pas
@@ -17,9 +17,9 @@ begin
 	F[1] := 1;
 
 	for N := Low(N) to High(N) do
-		WriteLn(IntToStr(N) + 'th term is processed;');
+		WriteLn(IntToStr(N+1) + 'th term is processed;');
 		Num1 := N-1;
 		Num2 := N-2;
 		F[N] := F[Num1] + F[Num2];
-		WriteLn(F[N]);
+		WriteLn(F[N]);  { Print N+1 th fibonacci sequence term }
 end.
