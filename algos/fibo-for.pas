479f2e6ca0e13ecddf9bf24e01921ec866bd563a
Ryohskay on Pop <learnerryo@gmail.com>
2023-03-24 23:50:47 +0000

    finally working! yaaayyyy

--- algos/fibo-for.pas
+++ algos/fibo-for.pas
@@ -5,7 +5,7 @@ program fibo_for;
 uses SysUtils;
 
 type
-	TFiboIdx = 2..49;
+	TFiboIdx = 2..24;
 
 var
 	F: array of Integer;
@@ -13,14 +13,16 @@ var
 	Num1, Num2: Integer;
 
 begin
-	SetLength(F, 50);
+	SetLength(F, 25);
 	F[0] := 0;
 	F[1] := 1;
 
 	for N := Low(N) to High(N) do
-		WriteLn(IntToStr(N+1) + 'th term is processed;');
-		Num1 := N-1;
-		Num2 := N-2;
-		F[N] := F[Num1] + F[Num2];
-		WriteLn(F[N]);  { Print N+1 th fibonacci sequence term }
+    begin
+		  WriteLn(IntToStr(N+1) + 'th term is processed;');
+		  Num1 := N-1;
+		  Num2 := N-2;
+		  F[N] := F[Num1] + F[Num2];
+		  WriteLn(F[N]);  { Print N+1 th fibonacci sequence term }
+    end;
 end.
