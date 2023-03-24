676f9653a9a0b4df027f618d6cf6614fe55b906e
Ryohskay on Pop <learnerryo@gmail.com>
2023-03-24 23:39:35 +0000

    cleaned up the code

--- algos/fibo-for.pas
+++ algos/fibo-for.pas
@@ -4,7 +4,7 @@ program fibo_for;
 uses SysUtils;
 
 type
-	TFiboIdx = 0..50;
+	TFiboIdx = 2..49;
 
 var
 	F: array of Integer;
@@ -16,17 +16,10 @@ begin
 	F[0] := 0;
 	F[1] := 1;
 
-	for N := Low(F) to High(F) do
+	for N := Low(N) to High(N) do
 		WriteLn(IntToStr(N) + 'th term is processed;');
-		if N = 0 then
-			F[N] := 0
-		else if N = 1 then
-			F[N] := 1
-		else
-			begin
-				Num1 := N-1;
-				Num2 := N-2;
-				F[N] := F[Num1] + F[Num2];
-				WriteLn(F[N]);
-			end;
+		Num1 := N-1;
+		Num2 := N-2;
+		F[N] := F[Num1] + F[Num2];
+		WriteLn(F[N]);
 end.
