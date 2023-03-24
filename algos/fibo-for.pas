15eac23546f544f8637b876393b69805ee8e109f
Ryohskay on Pop <learnerryo@gmail.com>
2023-03-24 23:55:48 +0000

    corrected and aligned tab formatting

--- algos/fibo-for.pas
+++ algos/fibo-for.pas
@@ -5,24 +5,24 @@ program fibo_for;
 uses SysUtils;
 
 type
-	TFiboIdx = 2..24;
+  TFiboIdx = 2..24;
 
 var
-	F: array of Integer;
-	N: TFiboIdx;
-	Num1, Num2: Integer;
+  F: array of Integer;
+  N: TFiboIdx;
+  Num1, Num2: Integer;
 
 begin
-	SetLength(F, 25);
-	F[0] := 0;
-	F[1] := 1;
+  SetLength(F, 25);
+  F[0] := 0;
+  F[1] := 1;
 
-	for N := Low(N) to High(N) do
+  for N := Low(N) to High(N) do
     begin
-		  WriteLn(IntToStr(N+1) + 'th term is processed;');
-		  Num1 := N-1;
-		  Num2 := N-2;
-		  F[N] := F[Num1] + F[Num2];
-		  WriteLn(F[N]);  { Print N+1 th fibonacci sequence term }
+      WriteLn(IntToStr(N+1) + 'th term is processed;');
+      Num1 := N-1;
+      Num2 := N-2;
+      F[N] := F[Num1] + F[Num2];
+      WriteLn(F[N]);  { Print N+1 th fibonacci sequence term }
     end;
 end.
