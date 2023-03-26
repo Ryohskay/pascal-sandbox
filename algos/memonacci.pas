ef51b89c5fd271ef630685ed6218687dc4369e31
Ryohskay on Pop <learnerryo@gmail.com>
2023-03-26 03:54:06 +0100

    successfully implemented a memoized recursive fibonacci calculation

--- algos/memonacci.pas
+++ algos/memonacci.pas
@@ -3,10 +3,21 @@ program memonacci;
 { Implementation of fibonacci sequence calculation by recursion with memoization }
 
 var
-  memo: array of int64;  { Dynamic array for memoization }
+  memo: array[2..50] of int64;  { Dynamic array for memoization }
+  I, J: Integer;  { Loop vars }
 
 function Fibo(N: Integer): int64;
+  var
+    Calc: int64;
+
   begin
+    { NB: N must be >=1 }
+    if N <= 0 then
+      begin
+        WriteLn('Fibo(N): N must be a positive integer (>=1)!');
+        Exit;
+      end;
+
     { Base cases }
     if (N = 0) then
       begin
@@ -20,10 +31,11 @@ function Fibo(N: Integer): int64;
       end;
     
     { Look for the value in memo }
-    if (memo[N] == -1) then
+    if (memo[N] = -1) then
       begin
-        Result := Fibo(N-1) + Fibo(N-2);
-        memo[N] := Result;
+        Calc := Fibo(N-1) + Fibo(N-2);
+        memo[N] := Calc;
+        Result := Calc;
       end
     else
       Result := memo[N];
@@ -31,5 +43,13 @@ function Fibo(N: Integer): int64;
 
 
 begin
-  SetLength(memo, 50);  { Instanciate memo by SetLength() }
-  for i := 0 to 49
+  for I := 2 to 50 do
+    memo[I] := -1;  { initialse all slot with -1 }
+
+  { Calculate N+1 th Fibonacci sequence term }
+  Fibo(49);
+
+  { The array memo now contains answers }
+  for J := 2 to 50 do
+    WriteLn(J+1, 'th term: ', memo[J], '!');
+end.
