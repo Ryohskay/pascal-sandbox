789b72361fe1093abe27cf3c651979483ee19639
Ryohskay on Pop <learnerryo@gmail.com>
2023-03-26 03:39:44 +0100

    simplified code to see how for loop exits in Pascal

--- foundations/for-loop.pas
+++ foundations/for-loop.pas
@@ -1,16 +1,8 @@
 program for_loop;
-
-type
-	TF = array[0..50] of Integer;
-	TIndex = 0..50;
-
 var
-	F: TF;
-	I: TIndex;
+  I: Integer;
 
 begin
-	F[0] := 0;
-	F[1] := 1;
 	for I := 0 to 49 do
 		WriteLn(I);
 end.
